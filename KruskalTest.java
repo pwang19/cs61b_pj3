9d61cc9b2c7658b8c3048c0649b3eb4c2946ff13
pwang19 <pwang19@gmail.com>
2014-04-28 21:47:57 -0700

    reverted file

--- KruskalTest.java
+++ KruskalTest.java
@@ -1,124 +1,176 @@
-/* Kruskal.java */
-
-package graphalg;
-
-import graph.*;
-import set.*;
+/* KruskalTest.java */
 
 /**
- * The Kruskal class contains the method minSpanTree(), which implements
- * Kruskal's algorithm for computing a minimum spanning tree of a graph.
- */
+* The KruskalTest class tests the Kruskal class.
+*/
 
-public class Kruskal {
+import graph.*;
+import graphalg.*;
+import java.util.*;
 
-  /**
-   * minSpanTree() returns a WUGraph that represents the minimum spanning tree
-   * of the WUGraph g.  The original WUGraph g is NOT changed.
-   *
-   * @param g The weighted, undirected graph whose MST we want to compute.
-   * @return A newly constructed WUGraph representing the MST of g.
-   */
-  public static WUGraph minSpanTree(WUGraph g){
-    WUGraph t = new WUGraph();
-    Object[] vertices = g.getVertices();
-    int size = vertices.length;
+public class KruskalTest {
 
-    for(int i = 0; i < size; i++){   
-      t.addVertex(vertices[i]);
-    }
-    LinkedQueue list = new LinkedQueue(); // don't know size needed
-    for(int i = 0; i < size; i++){
-      Neighbors vert = getNeighbors(vertices[i]);
-      for(int k = 0; k < vert.length; k++){
-        KruskalHelper temp = new KruskalHelper(vertices[i], vert.neighborList[k], vert.weightList[k]);
-        list.enqueue(temp);
-      }
-    }
-    
-    quickSort(list);
+  private static final int VERTICES = 10;
+  private static final int MAXINT = 100;
 
-    HashTableChained encodedVertices = new HashTableChained(13);
-    for(int i = 0; i < size; i++){
-      encodedVertices.insert(vertices[i],i);
-    }
+  private static boolean tree = true;
+  private static boolean minTree = true;
 
-    DisjointSets cycleCheck = new DisjointSets(size);
-    try{
-      while(!list.isEmpty()){
-        KruskalHelper temp = (KruskalHelper) list.dequeue();
-        int vertex1 = (int) encodedVertices.find(temp.getVertex1()).getWeight();
-        int vertex2 = (int) encodedVertices.find(temp.getVertex2()).getWeight();
-        int vertex1Root = cycleCheck.find(vertex1);
-        int vertex2Root = cycleCheck.find(vertex2);
-        if(vertex1Root != vertex2Root){
-          cycleCheck.union(vertex1, vertex2);
-          t.addedge(temp.getVertex1(), temp.getVertex2(), temp.getWeight());
-        } 
-      }
-    }catch(QueEmptyException e){
-      System.out.println("Wrong queue. minSpanTree");
-    }
-    return t;
-  }
+  public static void addRandomEdges(WUGraph g, Object[] vertArray) {
+    int i, j;
 
-  /**
-   *  quickSort() sorts q from smallest to largest using quicksort.
-   *  @param q is a LinkedQueue of Comparable objects.
-   **/
-  private static void quickSort(LinkedQueue q) {
-    // Your solution here.
-    if(q.size() == 0 || q.size() == 1){
-      return;
-    }
-    
-    int choose = (int) (Math.random() * q.size);
-    Comparable item = (Comparable) q.nth(choose + 1);
-    LinkedQueue qLarge = new LinkedQueue();
-    LinkedQueue qEquals = new LinkedQueue();
-    LinkedQueue qSmall = new LinkedQueue();
-    partition(q, item, qSmall, qEquals, qLarge);
-    quickSort(qSmall);
-    quickSort(qLarge);
-    q.append(qSmall);
-    q.append(qEquals);
-    q.append(qLarge);
-  }
+    System.out.println("Adding random edges to graph.");
 
-  /**
-   *  partition() partitions qIn using the pivot item.  On completion of
-   *  this method, qIn is empty, and its items have been moved to qSmall,
-   *  qEquals, and qLarge, according to their relationship to the pivot.
-   *  @param qIn is a LinkedQueue of Comparable objects.
-   *  @param pivot is a Comparable item used for partitioning.
-   *  @param qSmall is a LinkedQueue, in which all items less than pivot
-   *    will be enqueued.
-   *  @param qEquals is a LinkedQueue, in which all items equal to the pivot
-   *    will be enqueued.
-   *  @param qLarge is a LinkedQueue, in which all items greater than pivot
-   *    will be enqueued.  
-   **/   
-  public static void partition(LinkedQueue qIn, Comparable pivot, 
-                               LinkedQueue qSmall, LinkedQueue qEquals, 
-                               LinkedQueue qLarge) {
-    // Your solution here.
-    try{
-      while(!qIn.isEmpty()){
-        Comparable nextQue = (Comparable) qIn.dequeue();
-        int comparison = nextQue.compareTo(pivot);
-        if(comparison>0){
-          qLarge.enqueue(nextQue);
-        }else if(comparison < 0){
-          qSmall.enqueue(nextQue);
-        }else{
-          qEquals.enqueue(nextQue);
+    Random random = new Random(3); // Create a "Random" object with seed 0
+
+    for (i = 0; i < vertArray.length; i++) {
+      for (j = i; j < vertArray.length; j++) {
+        int r = random.nextInt() % MAXINT; // Between -99 and 99
+        if (r >= 0) {
+          g.addEdge(vertArray[i], vertArray[j], r);
         }
       }
-    }catch(QueEmptyException e){
-      System.out.println("Wrong queue. partition");
     }
   }
-  
 
-//disjoint sets - use hash table to map (new code for hashing)
+  public static void DFS(WUGraph t, DFSVertex current, DFSVertex prev,
+                         int[] maxOnPath, int maxEdge) {
+    Neighbors neigh;
+    int i;
+
+    current.visited = true;
+    maxOnPath[current.number] = maxEdge;
+    neigh = t.getNeighbors(current);
+    if (neigh != null) {
+      for (i = 0; i < neigh.neighborList.length; i++) {
+        DFSVertex next = (DFSVertex) neigh.neighborList[i];
+        if (next.visited) {
+          if ((next != current) && (next != prev)) {
+            tree = false;
+            return;
+          }
+        } else if (neigh.weightList[i] > maxEdge) {
+          DFS(t, next, current, maxOnPath, neigh.weightList[i]);
+        } else {
+          DFS(t, next, current, maxOnPath, maxEdge);
+        }
+        if (!tree) {
+          return;
+        }
+      }
+    }
+  }
+
+  public static void DFSTest(WUGraph g, WUGraph t, DFSVertex[] vertArray) {
+    int[][] maxOnPath;
+    Neighbors neigh;
+    int i, j;
+
+    System.out.println("Testing the tree.");
+
+    maxOnPath = new int[VERTICES][VERTICES];
+    for (i = 0; i < VERTICES; i++) {
+      for (j = 0; j < VERTICES; j++) {
+        vertArray[j].visited = false;
+      }
+      DFS(t, vertArray[i], null, maxOnPath[i], -MAXINT);
+      for (j = 0; j < VERTICES; j++) {
+        if (!vertArray[j].visited) {
+          tree = false;
+        }
+      }
+      if (!tree) {
+        return;
+      }
+    }
+
+// for (i = 0; i < vertArray.length; i++) {
+// for (j = 0; j < vertArray.length; j++) {
+// System.out.print(" " + maxOnPath[i][j]);
+// }
+// System.out.println();
+// }
+
+    for (i = 0; i < VERTICES; i++) {
+      neigh = g.getNeighbors(vertArray[i]);
+      if (neigh != null) {
+        for (j = 0; j < neigh.neighborList.length; j++) {
+          int v = ((DFSVertex) neigh.neighborList[j]).number;
+          if (neigh.weightList[j] < maxOnPath[i][v]) {
+            minTree = false;
+          }
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    int i, j;
+    int score;
+    WUGraph g, t;
+    DFSVertex[] vertArray;
+
+    System.out.println("Running minimum spanning tree test.");
+    System.out.println("Creating empty graph.");
+    g = new WUGraph();
+
+    System.out.println("Adding " + VERTICES + " vertices.");
+    vertArray = new DFSVertex[VERTICES];
+    for (i = 0; i < VERTICES; i++) {
+      vertArray[i] = new DFSVertex();
+      vertArray[i].number = i;
+      g.addVertex(vertArray[i]);
+    }
+
+    addRandomEdges(g, vertArray);
+
+// for (i = 0; i < vertArray.length; i++) {
+// for (j = 0; j < vertArray.length; j++) {
+// if (g.isEdge(vertArray[i], vertArray[j])) {
+// System.out.print(" " + g.weight(vertArray[i], vertArray[j]));
+// } else {
+// System.out.print(" *");
+// }
+// }
+// System.out.println();
+// }
+
+    System.out.println("Finding the minimum spanning tree.");
+    t = Kruskal.minSpanTree(g);
+
+// for (i = 0; i < vertArray.length; i++) {
+// for (j = 0; j < vertArray.length; j++) {
+// if (t.isEdge(vertArray[i], vertArray[j])) {
+// System.out.print(" " + t.weight(vertArray[i], vertArray[j]));
+// } else {
+// System.out.print(" *");
+// }
+// }
+// System.out.println();
+// }
+
+    DFSTest(g, t, vertArray);
+
+    if (tree) {
+      System.out.println("One point for creating a tree.");
+      if (minTree) {
+        System.out.println("Two points for creating a minimum spanning tree.");
+        score = 3;
+      } else {
+        System.out.println("Not a minimum spanning tree.");
+        score = 1;
+      }
+    } else {
+      System.out.println("Not a tree.");
+      score = 0;
+    }
+
+    System.out.println("Your Kruskal test score is " + score + " out of 3.");
+    System.out.println(" (Be sure also to run WUGTest.java.)");
+  }
+}
+
+class DFSVertex {
+  boolean visited;
+  int number;
 }
