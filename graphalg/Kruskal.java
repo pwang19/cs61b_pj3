f409afd9a13bc676e2b2c76588e5f0c882691448
jinhunkim <jinhunkim@berkeley.edu>
2014-04-30 09:00:23 -0700

    Kruskal and ListSorts

--- graphalg/Kruskal.java
+++ graphalg/Kruskal.java
@@ -2,11 +2,11 @@
 
 package graphalg;
 
-import list.LinkedQueue;
-import list.QueueEmptyException;
-import dict.HashTableChained;
 import graph.*;
 import set.*;
+import dict.*;
+import list.*;
+
 
 /**
  * The Kruskal class contains the method minSpanTree(), which implements
@@ -23,105 +23,60 @@ public class Kruskal {
    * @return A newly constructed WUGraph representing the MST of g.
    */
   public static WUGraph minSpanTree(WUGraph g){
-    WUGraph t = new WUGraph();
-    Object[] vertices = g.getVertices();
-    int size = vertices.length;
+    // new graph to store minSpanTree
+  	WUGraph t = new WUGraph();
+  	Object[] vertices = g.getVertices();
+  	int size = vertices.length;
 
-    for(int i = 0; i < size; i++){   
-      t.addVertex(vertices[i]);
-    }
-    LinkedQueue list = new LinkedQueue(); // don't know size needed
-    for(int i = 0; i < size; i++){
-      Neighbors vert = getNeighbors(vertices[i]);
-      for(int k = 0; k < vert.neighborList.length; k++){
-        KruskalHelper temp = new KruskalHelper(vertices[i], vert.neighborList[k], vert.weightList[k]);
-        list.enqueue(temp);
-      }
-    }
+    // store vertexes from g to t
+  	for(int i = 0; i < size; i++){   
+  		t.addVertex(vertices[i]);
+  	}
+
+    // don't know size needed so use LinkedQueue
+  	LinkedQueue list = new LinkedQueue();
+
+    // Store all edges from g into a LinkedQueue using getNeighbors()
+  	for(int i = 0; i < size; i++){
+  		Neighbors vert = getNeighbors(vertices[i]);
+  		for(int k = 0; k < vert.length; k++){
+  			KruskalHelper temp = new KruskalHelper(vertices[i], vert.neighborList[k], vert.weightList[k]);
+  			list.enqueue(temp);
+  		}
+  	}
+  	
+    // Sort the list using quicksort to achieve  O(|E|log(|E|)) 
+  	mergeSort(list);
+
+    // Use HashTableChained to map objects that serve as Vertices to unique integers
+    // 
+  	HashTableChained encodedVertices = new HashTableChained();
+  	for(int i = 0; i < size; i++){
+  		encodedVertices.insert(vertices[i],i);
+  	}
+
+    // Use DisjointSets to make sure no cycle 
+  	DisjointSets cycleCheck = new DisjointSets(size);
+
     
-    quickSort(list);
-
-    HashTableChained encodedVertices = new HashTableChained(13);
-    for(int i = 0; i < size; i++){
-      encodedVertices.insert(vertices[i],i);
-    }
-
-    DisjointSets cycleCheck = new DisjointSets(size);
     try{
-      while(!list.isEmpty()){
-        KruskalHelper temp = (KruskalHelper) list.dequeue();
-        int vertex1 = (int) encodedVertices.find(temp.getVertex1()).getWeight();
-        int vertex2 = (int) encodedVertices.find(temp.getVertex2()).getWeight();
-        int vertex1Root = cycleCheck.find(vertex1);
-        int vertex2Root = cycleCheck.find(vertex2);
-        if(vertex1Root != vertex2Root){
+    	while(!list.isEmpty()){
+    		KruskalHelper temp = (KruskalHelper) list.dequeue();
+    		int vertex1 = (int) encodedVertices.find(temp.getVertex1()).value();
+    		int vertex2 = (int) encodedVertices.find(temp.getVertex2()).value();
+
+        int vertex1Root = (int) cycleCheck.find(vertex1);
+        int vertex2Root = (int) cycleCheck.find(vertex2);
+    		if(vertex1Root != vertex2Root){
           cycleCheck.union(vertex1, vertex2);
-          t.addEdge(temp.getVertex1(), temp.getVertex2(), temp.getWeight());
+          t.addEdge(vertex1, vertex2, temp.getWeight());
         } 
-      }
-    }catch(QueueEmptyException e){
+    	}
+    }catch(QueEmptyException e){
       System.out.println("Wrong queue. minSpanTree");
     }
     return t;
   }
 
-  /**
-   *  quickSort() sorts q from smallest to largest using quicksort.
-   *  @param q is a LinkedQueue of Comparable objects.
-   **/
-  private static void quickSort(LinkedQueue q) {
-    // Your solution here.
-    if(q.size() == 0 || q.size() == 1){
-      return;
-    }
-    
-    int choose = (int) (Math.random() * q.size());
-    Comparable item = (Comparable) q.nth(choose + 1);
-    LinkedQueue qLarge = new LinkedQueue();
-    LinkedQueue qEquals = new LinkedQueue();
-    LinkedQueue qSmall = new LinkedQueue();
-    partition(q, item, qSmall, qEquals, qLarge);
-    quickSort(qSmall);
-    quickSort(qLarge);
-    q.append(qSmall);
-    q.append(qEquals);
-    q.append(qLarge);
-  }
 
-  /**
-   *  partition() partitions qIn using the pivot item.  On completion of
-   *  this method, qIn is empty, and its items have been moved to qSmall,
-   *  qEquals, and qLarge, according to their relationship to the pivot.
-   *  @param qIn is a LinkedQueue of Comparable objects.
-   *  @param pivot is a Comparable item used for partitioning.
-   *  @param qSmall is a LinkedQueue, in which all items less than pivot
-   *    will be enqueued.
-   *  @param qEquals is a LinkedQueue, in which all items equal to the pivot
-   *    will be enqueued.
-   *  @param qLarge is a LinkedQueue, in which all items greater than pivot
-   *    will be enqueued.  
-   **/   
-  public static void partition(LinkedQueue qIn, Comparable pivot, 
-                               LinkedQueue qSmall, LinkedQueue qEquals, 
-                               LinkedQueue qLarge) {
-    // Your solution here.
-    try{
-      while(!qIn.isEmpty()){
-        Comparable nextQue = (Comparable) qIn.dequeue();
-        int comparison = nextQue.compareTo(pivot);
-        if(comparison>0){
-          qLarge.enqueue(nextQue);
-        }else if(comparison < 0){
-          qSmall.enqueue(nextQue);
-        }else{
-          qEquals.enqueue(nextQue);
-        }
-      }
-    }catch(QueueEmptyException e){
-      System.out.println("Wrong queue. partition");
-    }
-  }
-  
-
-//disjoint sets - use hash table to map (new code for hashing)
 }
---
+++ graphalg/ListSorts.java
@@ -0,0 +1,119 @@
+/* ListSorts.java */
+
+import list.*;
+
+public class ListSorts {
+
+  private final static int SORTSIZE = 1000;
+
+  /**
+   *  makeQueueOfQueues() makes a queue of queues, each containing one item
+   *  of q.  Upon completion of this method, q is empty.
+   *  @param q is a LinkedQueue of objects.
+   *  @return a LinkedQueue containing LinkedQueue objects, each of which
+   *    contains one object from q.
+   **/
+  public static LinkedQueue makeQueueOfQueues(LinkedQueue q) {
+    // Replace the following line with your solution.
+    LinkedQueue queofque = new LinkedQueue();
+    try{
+      while(!q.isEmpty()){
+        Comparable item = (Comparable) q.dequeue();
+        LinkedQueue que = new LinkedQueue();
+        que.enqueue(item);
+        queofque.enqueue(que);
+      }
+    }catch(QueEmptyException e){
+      System.out.println("Wrong queue. makeQueueOfQueues");
+    }
+    return queofque;
+  }
+
+  /**
+   *  mergeSortedQueues() merges two sorted queues into a third.  On completion
+   *  of this method, q1 and q2 are empty, and their items have been merged
+   *  into the returned queue.
+   *  @param q1 is LinkedQueue of Comparable objects, sorted from smallest 
+   *    to largest.
+   *  @param q2 is LinkedQueue of Comparable objects, sorted from smallest 
+   *    to largest.
+   *  @return a LinkedQueue containing all the Comparable objects from q1 
+   *   and q2 (and nothing else), sorted from smallest to largest.
+   **/
+  public static LinkedQueue mergeSortedQueues(LinkedQueue q1, LinkedQueue q2) {
+    // Replace the following line with your solution.
+    LinkedQueue q3 = new LinkedQueue();
+    try{
+      while(!q1.isEmpty() || !q2.isEmpty()){
+        Comparable itemq1 = (Comparable) q1.front();
+        Comparable itemq2 = (Comparable) q2.front();
+        int comparison = itemq1.compareTo(item.q2)
+        if(comparison>0){
+          q3.enqueue(itemq2);
+          q2.dequeue();
+        }else{
+          q3.enqueue(itemq1);
+          q1.dequeue();
+        }
+      }
+      if(q1.isEmpty()){
+        q3.append(q2);
+      }else{
+        q3.append(q1);
+      }
+    }catch(QueEmptyException e){
+      System.out.println("Wrong queue. mergeSortedQueues");
+    }
+    return q3;
+  }
+
+  
+  /**
+   *  mergeSort() sorts q from smallest to largest using mergesort.
+   *  @param q is a LinkedQueue of Comparable objects.
+   **/
+  public static void mergeSort(LinkedQueue q) {
+    // Your solution here.
+    LinkedQueue mergeQueofQue = q.makeQueueOfQueues();
+    try{
+      while(mergeQueofQue.size() > 1){
+        LinkedQueue q1 = (LinkedQueue) mergeQueofQue.dequeue();
+        LinkedQueue q2 = (LinkedQueue) mergeQueofQue.dequeue();
+        LinkedQueue q3 = mergeSortedQueues(q1, q2);
+        q1.enqueue(q3);
+      }
+      q.append()
+    }catch(QueEmptyException e){
+      System.out.println("Wrong queue. merge");
+    }
+  }
+
+
+  /**
+   *  makeRandom() builds a LinkedQueue of the indicated size containing
+   *  Integer items.  The items are randomly chosen between 0 and size - 1.
+   *  @param size is the size of the resulting LinkedQueue.
+   **/
+  public static LinkedQueue makeRandom(int size) {
+    LinkedQueue q = new LinkedQueue();
+    for (int i = 0; i < size; i++) {
+      q.enqueue(new Integer((int) (size * Math.random())));
+    }
+    return q;
+  }
+
+  /**
+   *  main() performs some tests on mergesort and quicksort.  Feel free to add
+   *  more tests of your own to make sure your algorithms works on boundary
+   *  cases.  Your test code will not be graded.
+   **/
+  public static void main(String [] args) {
+
+    LinkedQueue q = makeRandom(10);
+    System.out.println(q.toString());
+    mergeSort(q);
+    System.out.println(q.toString());
+
+  }
+
+}
